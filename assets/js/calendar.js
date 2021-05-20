f8d263087d9e7384cd45a7b74bd7b1e90cb41dc6
Sonika <sonikapoudyal5@gmail.com>
2021-05-20 12:51:24 -0400

    added some styles to the page

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,3 +1,21 @@
 var convertedDate = moment().format('dddd, MMMM Do YYYY');
-
 $("#currentDay").html(convertedDate);
+
+// var timeInputs = [
+//     {time: 8, input: ""},
+//     {time: 9, input: ""},
+//     {time: 10, input: ""},
+//     {time: 11, input: ""},
+//     {time: 12, input: ""},
+//     {time: 1, input: ""},
+//     {time: 2, input: ""},
+//     {time: 3, input: ""},
+//     {time: 4, input: ""},
+//     {time: 5, input: ""},
+//     {time: 6, input: ""}
+// ]
+
+$("#description").click(function(){
+    var saveButton = $("#description");
+    console.log(saveButton);
+})
